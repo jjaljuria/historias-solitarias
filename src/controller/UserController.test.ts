0ff05a2b0bb90a5e3a9445d9c3ed74f211241ee1
José Aljuria <josejavieral13@gmail.com>
2022-11-28 15:00:39 -0400

    first commit

---
+++ src/controller/UserController.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect ,it} from "vitest";
+import UserController from './UserController';
+
+describe('UserController', ()=>{
+	it('UserController.ts has been defined', ()=>{
+		expect(UserController).toBeDefined()
+	})
+
+	it('should UserController has ')
+})
