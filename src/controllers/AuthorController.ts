aed311417946a9df889703838f2e77ab11696b50
José Aljuria <josejavieral13@gmail.com>
2022-11-29 15:24:37 -0400

    updated without importance

--- src/controllers/AuthorController.ts
+++ src/controllers/AuthorController.ts
@@ -3,7 +3,6 @@ import { RequestHandler } from "express";
 
 export const getAuthor: RequestHandler = async (req, res) => {
   const { id } = req.params;
-  console.log(id);
   const author = await Author.findById(id);
   return res.json(author);
 };
--- src/models/Author.ts
+++ src/models/Author.ts
@@ -1,26 +1,39 @@
-import { Schema, model, connect, ObjectId } from 'mongoose';
-import {v4} from 'uuid'
+import { Schema, model, ObjectId } from "mongoose";
+import bcrypt from "bcryptjs";
 
-export interface IAuthor{
-	_id: ObjectId;
-	name: string;
-	password: string;
-	description: string;
+export interface IAuthor {
+  _id: ObjectId;
+  name: string;
+  password: string;
+  description: string;
 }
 
-const authorSchema: Schema = new Schema<IAuthor>({
-	name: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	description: String
-},{
-	timestamps: true,
-})
+const authorSchema: Schema = new Schema<IAuthor>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    description: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const Author = model<IAuthor>('Author',authorSchema)
+authorSchema.methods.encryptPassword = async (password: string) => {
+  const salt = await bcrypt.genSalt(100);
+  const hash = bcrypt.hash(password, salt);
+  return hash;
+};
+
+authorSchema.methods.matchPassword = async function (password: string) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const Author = model<IAuthor>("Author", authorSchema);
